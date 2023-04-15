3837d345517b960db87ac5478cb45267047fad1e
Paul Morrow <morrowp@sheridancollege.ca>
2023-04-14 20:37:09 -0400

    Test.

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -4,30 +4,30 @@ Fibonacci test
 
 from fibonacci import FibonacciRange
 
-def test_str():
+#def test_str():
   
-  assert [number for number in FibonacciRange("Hello")] == ValueError
+  #assert [number for number in FibonacciRange("Hello")] == ValueError
 
-def test_zero():
+#def test_zero():
   
-  assert [number for number in FibonacciRange(0)] == [0]
+  #assert [number for number in FibonacciRange(0)] == [0]
   
-def test_one():
+#def test_one():
   
-  assert [number for number in FibonacciRange(1)] == [0, 1]
+  #assert [number for number in FibonacciRange(1)] == [0, 1]
   
 def test_two():
   
   assert [number for number in FibonacciRange(2)] == [0, 1, 1]
   
-def test_four():
+#def test_four():
   
-  assert [number for number in FibonacciRange(4)] == [0, 1, 1, 2, 3]
+  #assert [number for number in FibonacciRange(4)] == [0, 1, 1, 2, 3]
   
-def test_ten():
+#def test_ten():
   
-  assert [number for number in FibonacciRange(10)] == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+  #assert [number for number in FibonacciRange(10)] == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
   
-def test_negative():
+#def test_negative():
   
-  assert [number for number in FibonacciRange(-4)] == []
+  #assert [number for number in FibonacciRange(-4)] == []
